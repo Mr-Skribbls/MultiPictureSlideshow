959f3b3cdaadf52519febfceb78f3fbcfcaff370
Mr-Skribbls <brianjensen@Brians-MacBook-Pro.local>
2015-11-10 23:14:53 -0700

    update

--- script.js
+++ script.js
@@ -1,106 +1,134 @@
-//Requires jQuery
-
 $(document).ready(function() {
-    var id_array = [1, 2, 3], random;
-    var bg_array = ["bg1", "bg2", "bg3", "bg4", "bg5", "bg6", "bg7", "bg8", "bg9", "bg10", "bg11"]; //add as many background classes as you like. Each bg represents an image
-    var bg_on = ["bg1", "bg2", "bg3", "bg4", "bg5", "bg6", "bg7", "bg8"] //images displayed when the page first loads.
-    var bg_off = ["bg9", "bg10", "bg11"] //images not displayed when the page first loads.
     
-    setInterval(function () {
-        random = Math.floor((Math.random() * 2) + 1);
+    var image_folder = 'imgs';
+    
+    var bg_array = [], bg_array_on = [], bg_array_off = [];
+    
+    create_img_RegEx(image_folder);
+    populate_bg_array();
+    populate_bg_array_on();
+    populate_bg_array_off();
+    buildInitHTMl();
+    setInterval(function() {
+        picture_switcher();
+    }, 3000);
+    
+    
+    
+    // ----- Functions ----- //
+    
+    // Picture Change function
+    function picture_switcher() {
+        var random = Math.floor((Math.random() * 3) + 1); // random number from 1 to 3
+        var el_to_remove = $('#t' + random);
+        var el_to_remove_classes = el_to_remove.attr('class').split(' ');
         
-        if(random === 2) {
-            picture_switcher("#t", "#top");
+        // large images
+        // test if element class containes 'large'
+        if( $.inArray('large', el_to_remove_classes) !== -1 ) {
+            var el_to_remove_img = el_to_remove.css('background-image').match(img_RegEx);
             
-        }
+            // remove img from bg_array_on to bg_array_off
+            bg_array_on.splice(bg_array_on.indexOf(el_to_remove_img[0]), 1);
+            bg_array_off.push(el_to_remove_img[0]);
+
+            // add new img to bg_array_on from bg_array_off
+            bg_array_on.push(bg_array_off[0]);
+            bg_array_off.splice(0,1);
+            
+            // create new img html
+            var new_img_html = '<div id="t' + random + '" class="box large" style="background-image: url(\'' + bg_array_on[3] + '\')"></div>';
+            
+        } 
+        // small images
+        // element class does not contain 'large'
         else {
-            picture_switcher("#b", "#bottom");
+            var el_to_remove_img_1 = $('#t' + random + ' > div').css('background-image').match(img_RegEx);
+            var el_to_remove_img_2 = $('#t' + random + ' div:nth-child(2)').css('background-image').match(img_RegEx);
+            
+            // remove both images from bg_array_on to bg_array_off
+            bg_array_on.splice(bg_array_on.indexOf(el_to_remove_img_1[0]), 1);
+            bg_array_off.push(el_to_remove_img_1[0]);
+            bg_array_on.splice(bg_array_on.indexOf(el_to_remove_img_2[0]), 1);
+            bg_array_off.push(el_to_remove_img_2[0]);
+            
+            // add 2 new images to bg_array_on from bg_array_off
+            bg_array_on.push(bg_array_off[0]);
+            bg_array_off.splice(0,1);
+            bg_array_on.push(bg_array_off[0]);
+            bg_array_off.splice(0,1);
+            
+            // create new img html
+            var new_img_html = '';
+            new_img_html += '<div id="t' + random + '" class="box slider-small-container">';
+            new_img_html += '<div class="small" style="background-image: url(\'' + bg_array_on[2] + '\')"></div>';
+            new_img_html += '<div class="small" style="background-image: url(\'' + bg_array_on[3] + '\')"></div>';
+            new_img_html += '</div>';
+            
         }
-    }, 3500);
-    
-    function picture_switcher (a, b) {
-        random = Math.floor((Math.random() * 3));
-            var old_id = a + id_array[random];
-            
-            // get the contents and attributes of the random element
-            var el_id = $(old_id).attr('id');
-            var el_class = $(old_id).attr('class');
-            var el_contents = $(old_id).html();
-            
-            // find the bg class that is in el_class
-            var el_classes_array = el_class.split(" ");
-            
-            var el_bg_class = "none";
-            for (i = 0; i < bg_array.length; i++) {
-                var on_test = $.inArray(bg_array[i], el_classes_array);
-                if (on_test != -1) {
-                    el_bg_class = el_classes_array[on_test];
-                    
-                    // replace it with a bg class that is in bg_off
-                    el_class = el_class.replace(el_bg_class, bg_off[0]);
-                    
-                    // remove the old bg class from bg_on and add it to bg_off
-                    bg_on.splice($.inArray(el_bg_class, bg_on), 1, bg_off[0]);
-                    
-                    // remove the new bg class from bg_off and add it to bg_on
-                    bg_off.splice($.inArray(bg_off[0], bg_off), 1);
-                    bg_off.push(el_bg_class);
-                    break;
-                }
-            }
-            
-            if (!isset(el_bg_class) || el_bg_class === "none") {
-                var el_class_1 = $(old_id + " > div").attr('class');
-                var el_class_2 = $(old_id + " div:nth-child(2)").attr('class');
-                
-                var el_classes_1_array = el_class_1.split(" ");
-                var el_classes_2_array = el_class_2.split(" ");
-                
-                for (i = 0; i < bg_array.length; i++) {
-                    var on_test_1 = $.inArray(bg_array[i], el_classes_1_array);
-                    var on_test_2 = $.inArray(bg_array[i], el_classes_2_array);
-                    
-                    if(on_test_1 != -1) {
-                        var el_bg_class_1 = el_classes_1_array[on_test_1];
-                        el_class_1 = el_class_1.replace(el_bg_class_1, bg_off[0]);
-                        bg_on.splice($.inArray(el_bg_class_1, bg_on), 1, bg_off[0]);
-                        bg_off.splice($.inArray(bg_off[0], bg_off), 1);
-                        bg_off.push(el_bg_class_1);
-                    }
-                    if(on_test_2 != -1) {
-                        var el_bg_class_2 = el_classes_2_array[on_test_2];
-                        el_class_2 = el_class_2.replace(el_bg_class_2, bg_off[0]);
-                        bg_on.splice($.inArray(el_bg_class_2, bg_on), 1, bg_off[0]);
-                        bg_off.splice($.inArray(bg_off[0], bg_off), 1);
-                        bg_off.push(el_bg_class_2);
-                    }
-                    
-                }
-                
-                new_id = "new";
-                var el_build_sub = "<div id='" + new_id + "' class='" + el_class + "'><div class='" + el_class_1 + "'></div><div class='" + el_class_2 + "'></div>" +el_contents + "</div>";
-            }
-            
-            var new_id = "new";
-            var el_build = "<div id='" + new_id + "' class='" + el_class + "'>" + el_contents + "</div>";
-            if(!isset(el_build_sub)) {
-                $(b).append(el_build);
-            }
-            else {
-                $(b).append(el_build_sub);
-            }
-            
-            $(old_id).css("width", "0px");
-            
-            // remove the random element
-            setTimeout(function() {
-                $(old_id).remove();
-                // change new element id to the old id
-                $("#" + new_id).attr("id", el_id);
-            },2000);
+        
+        // add new img html to slide show
+        $('#top').append(new_img_html)
+        
+        // shrink img to remove 
+        el_to_remove.css('width', '0px');
+
+        // remove old img
+        setInterval(function() {
+            el_to_remove.remove();
+        }, 2000);
     }
     
-        // ----- Isset function ------ //
+    // build initial html
+    function buildInitHTMl() {
+        var html_contents = '';
+        html_contents += '<div class="slider-row">';
+        html_contents += '    <div id="top" class="slider">';
+        html_contents += '        <div id="t1" class="box large" style="background-image: url(\'' + bg_array_on[0] + '\')"></div>';
+        html_contents += '        <div id="t2" class="box slider-small-container">';
+        html_contents += '            <div class="small" style="background-image: url(\'' + bg_array_on[1] + '\')"></div>';
+        html_contents += '            <div class="small" style="background-image: url(\'' + bg_array_on[2] + '\')"></div>';
+        html_contents += '        </div>';
+        html_contents += '        <div id="t3" class="box large" style="background-image: url(\'' + bg_array_on[3] + '\')"></div>';
+        html_contents += '    </div>';
+        html_contents += '</div>';
+        
+        $('.slider-container').html(html_contents);
+    }
+    
+    // populate bg_array
+    function populate_bg_array() {
+        
+        // get images from html slider-container class
+        $.each($('.slider-container img'), function() {
+            bg_array.push($(this).attr('src'));
+        });
+        
+    }
+    
+    // populate bg_array_on
+    // first 4 objects in ob_array
+    function populate_bg_array_on() {
+        for( var i = 0; i < 4; i++ ) {
+            bg_array_on.push(bg_array[i]);
+        }
+    }
+    
+    // poulate bg_array_off
+    // all objects in bg_array not in bg_array_on
+    function populate_bg_array_off() {
+        for( var i = 0; i < bg_array.length; i++ ) {
+             if( ($.inArray(bg_array[i], bg_array_on)) === -1 ) bg_array_off.push(bg_array[i]);
+        }
+    }
+    
+    // build RegEx to filter image src
+    function create_img_RegEx(folder) {
+        folder = '(' + folder + '\/)(.*)(jpg|JPG|jpeg|gif|png)';
+        img_RegEx = new RegExp(folder)
+    }
+    
+    // check if an object is set
     function isset() {
         var a = arguments,
             l = a.length,
@@ -122,6 +150,5 @@ $(document).ready(function() {
           }
           return true;
     }
-    
     
 });
